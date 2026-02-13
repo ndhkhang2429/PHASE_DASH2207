99884af7aa3ddca3826cc0a393da95901ba0d12e
Hoang Khang <khangnguyen22072006@gmail.com>
2026-02-13 23:07:43 +0700

    Tao man hinh menu, pause, game over nhung chua hien duoc

--- Assets/Script/Controllers/GameManager.cs
+++ Assets/Script/Controllers/GameManager.cs
@@ -5,14 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
     public static GameManager Instance; //signleton pattern
-    public bool IsGameOver { get; private set; }
+    
     public GameObject gameOverPanel;
+    public GameObject gamePausedPanel;
+
+    public bool IsGameOver { get; private set; }
+    private bool IsGamePause = false;
 
     //Để mỗi lần vào GameScene nó tự reset.
     private void Start()
     {
         Time.timeScale = 1f;
+        IsGameOver = false;
+        IsGamePause = false;
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+
+        if (gamePausedPanel != null)
+            gamePausedPanel.SetActive(false);
     }
 
     private void Awake()
@@ -26,6 +39,44 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !IsGameOver)
+        {
+            if(IsGamePause)
+            {
+                UnpauseGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        IsGamePause = true;
+        Time.timeScale = 0f;
+        if (gamePausedPanel != null)
+            gamePausedPanel.SetActive(true);
+
+    }
+
+    public void UnpauseGame()
+    {
+        IsGamePause = false;
+        Time.timeScale = 1f;
+        gamePausedPanel.SetActive(false);
+    }
+
+    //Singleton phai clear khi object bi destroy
+    private void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
     }
 
     public void GameOver()
@@ -44,12 +95,14 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         Time.timeScale = 1f;
+        IsGameOver = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 
     public void OnClickQuitButton()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
     }
 }
