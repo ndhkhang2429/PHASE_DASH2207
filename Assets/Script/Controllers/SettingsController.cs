14b98c8505fd4c539265caaae54f82dc69cbc807
Nguyen Dang Hoang Khang <khangnguyen22072006@gmail.com>
2026-03-19 09:22:14 +0700

    Complete settings menu

---
+++ Assets/Script/Controllers/SettingsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    [Header("UI sliders")]
+    public Slider bgmSlider;
+    public Slider sfxSlider;
+
+    void Start()
+    {
+        if (bgmSource != null && bgmSlider != null)
+        {
+            bgmSlider.value = bgmSource.volume;
+        }
+
+        if (sfxSource != null && sfxSlider != null)
+        {
+            sfxSlider.value = sfxSource.volume;
+        }
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        if (bgmSource != null)
+            bgmSource.volume = volume;
+    }
+    public void SetSFXVolume(float volume)
+    {
+        if (sfxSource != null)
+            sfxSource.volume = volume;
+    }
+}
